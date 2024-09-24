fd1a34e7239f378675d1d6b2745370fce37e80d7
Shashwat-11321 <Shashwat.11321@stu.upes.ac.in>
2024-09-24 03:59:04 +0000

    This app is based on Gui Download manager

--- Downloader/Py_Downloader.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def download_file(url , file_name):
-    respo=requests.get(url)
-    if respo.status_code ==200:
-        with open(file_name , 'wb') as file:
-            Prit(hello)
